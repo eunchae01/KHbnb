e5147646ebf4d60a662f2dba137fa0b60f01ccf3
201815069 <70687333+eunchae01@users.noreply.github.com>
2022-06-22 23:26:45 +0900

    test

---
+++ AirKH/src/main/java/com/air/kyk/ReviewDAOm.java
@@ -0,0 +1,24 @@
+package com.air.kyk;
+
+import java.util.List;
+
+public interface ReviewDAOm {
+
+
+
+	public int getReivewListCount(int no);		// 숙소 번호에 따른 리뷰 갯수 조회
+
+	public List<ReviewDTOm> reviewCont(int no);		// 숙소번호에 따른 리뷰 조회
+
+	public AvgDTO avgCont(int no);
+
+	public List<ReviewDTOm> hostReviewCont(int no);
+
+
+	public int insertReview();					// 리뷰 생성
+
+	public int updateReview(ReviewDTOm dto);		// 리뷰 수정
+
+	public int deleteReview(int no);			// 리뷰 삭제
+
+}
---
+++ AirKH/src/main/webapp/WEB-INF/views/kyk/review_insert.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<form action="">
+
+		<table border="1" cellspacing="0" width="600">
+
+		</table>
+
+	</form>
+</body>
+</html>
